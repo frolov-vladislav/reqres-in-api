b1ca9a3d7ae319c9131edc8a6bbe6951fd03d6c6
frolov-vlad <frolov-vlad@toloka.ai>
2023-09-27 09:29:31 +0400

    add reqres api tests

--- src/test/java/com/frolov/tests/reqres/TestBase.java
+++ src/test/java/com/frolov/tests/reqres/TestBase.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
     @BeforeAll
     static  void up() {
         RestAssured.baseURI = "https://reqres.in/";
--- src/test/java/com/frolov/tests/reqres/TestData.java
+++ src/test/java/com/frolov/tests/reqres/TestData.java
@@ -3,6 +3,7 @@ package com.frolov.tests.reqres;
 import com.github.javafaker.Faker;
 
 public class TestData {
+
     public static final Faker faker = new Faker();
     public static final String name = "faker.name().firstName()";
     public static final String job = faker.job().title();
